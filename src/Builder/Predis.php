d65872d8c624b2bbcdb38070ef02ce9e46d99124
Jip Moors <jhp.moors@gmail.com>
2017-04-07 19:37:19 +0200

    Pass config as argument

--- src/Builder/Predis.php
+++ src/Builder/Predis.php
@@ -8,9 +8,6 @@ use MultiObjectCache\Cache\PoolBuilderInterface;
 use Predis\Client;
 
 class Predis implements PoolBuilderInterface {
-	/** @var array Configuration */
-	protected $config = [];
-
 	/**
 	 * Creates a pool
 	 *
@@ -19,34 +16,36 @@ class Predis implements PoolBuilderInterface {
 	 * @return AbstractCachePool
 	 * @throws \Exception
 	 */
-	public function create( array $config = array() ) {
-		$this->config = $config;
+	public function create( array $config = [] ) {
 
-		$predis = $this->connect();
+		$predis = $this->initialize( $config );
 
 		return new PredisCachePool( $predis );
 	}
 
 	/**
+	 * Initializes the Predis client.
+	 *
+	 * @param array $config
+	 *
 	 * @return Client
-	 * @throws \Exception
 	 */
-	private function connect() {
-		$options    = array();
+	private function initialize( array $config ) {
+		$options    = [];
 		$parameters = null;
 
-		if ( ! empty( $this->config['cluster'] ) ) {
-			$parameters         = $this->config['cluster'];
+		if ( ! empty( $config['cluster'] ) ) {
+			$parameters         = $config['cluster'];
 			$options['cluster'] = 'redis';
 		}
 
-		if ( ! empty( $this->config['servers'] ) ) {
-			$parameters             = $this->config['servers'];
+		if ( ! empty( $config['servers'] ) ) {
+			$parameters             = $config['servers'];
 			$options['replication'] = true;
 		}
 
-		if ( null !== $parameters && ! empty( $this->config['password'] ) ) {
-			$options['parameters']['password'] = $this->config['password'];
+		if ( null !== $parameters && ! empty( $config['password'] ) ) {
+			$options['parameters']['password'] = $config['password'];
 		}
 
 		$redis = new Client( $parameters, $options );
