1671583ca40feeb7644d0cc5f73f9974244a5953
Jip Moors <jhp.moors@gmail.com>
2017-04-08 17:36:43 +0200

    Add exception and re-order methods

--- src/Builder/Memcached.php
+++ src/Builder/Memcached.php
@@ -13,6 +13,7 @@ class Memcached implements PoolBuilderInterface {
 	 * @param array $config Config to use to create the pool.
 	 *
 	 * @return AbstractCachePool
+	 * @throws \Exception
 	 */
 	public function create( array $config = [] ) {
 		$config = wp_parse_args( $config, [
@@ -21,72 +22,11 @@ class Memcached implements PoolBuilderInterface {
 
 		$memcached = $this->createInstance( $config );
 
-		$this->addServers( $memcached, $this->getServers( $config ) );
+		if ( ! $this->addServers( $memcached, $this->getServers( $config ) ) ) {
+			throw new \RuntimeException( 'Memcached failed to add servers to the configuration.' );
+		}
 
 		return new MemcachedCachePool( $memcached );
-	}
-
-	/**
-	 * Adds an array of servers to the pool.
-	 *
-	 * Each individual server in the array must include a domain and port, with an optional
-	 * weight value: $servers = array( array( '127.0.0.1', 11211, 0 ) );
-	 *
-	 * @link    http://www.php.net/manual/en/memcached.addservers.php
-	 *
-	 * @param \Memcached $memcached Memcached instance.
-	 * @param array      $servers   Array of server to register.
-	 *
-	 * @return bool True on success; false on failure.
-	 */
-	protected function addServers( \Memcached $memcached, array $servers ) {
-		$add = $servers;
-
-		if ( $memcached->isPersistent() ) {
-			$add = $this->getUnusedServers( $memcached, $servers );
-		}
-
-		if ( ! empty( $add ) ) {
-			return $memcached->addServers( $add );
-		}
-
-		return true;
-	}
-
-	/**
-	 * Filters out used servers from a list
-	 *
-	 * @param \Memcached $memcached Memcached instance.
-	 * @param array      $servers   List of servers to filter out used ones from.
-	 *
-	 * @return array List of unused server.
-	 */
-	private function getUnusedServers( \Memcached $memcached, array $servers ) {
-		$unused = $servers;
-
-		$listed = $memcached->getServerList();
-		if ( ! empty( $listed ) ) {
-			$unused = array();
-
-			foreach ( $servers as $server ) {
-
-				$test = array(
-					'host'   => $server[0],
-					'port'   => $server[1],
-					'weight' => isset( $server[2] ) ? (int) $server[2] : 0,
-				);
-
-				$found = in_array( $test, $listed, true );
-
-				if ( $found ) {
-					continue;
-				}
-
-				$unused[] = $server;
-			}
-		}
-
-		return $unused;
 	}
 
 	/**
@@ -114,18 +54,77 @@ class Memcached implements PoolBuilderInterface {
 	protected function getServers( array $config ) {
 		$servers = [];
 
+		// Configuration supplied a single server.
 		if ( ! empty( $config['server'] ) ) {
 			$servers = [ $config['server'] ];
 		}
 
-		if ( ! emptY( $config['servers'] ) ) {
+		// Configuration supplied list of servers.
+		if ( ! empty( $config['servers'] ) ) {
 			$servers = $config['servers'];
 		}
 
-		if ( empty( $servers ) ) {
-			$servers = [ [ '127.0.0.1', 11211 ] ];
+		return $servers;
+	}
+
+	/**
+	 * Adds an array of servers to the pool.
+	 *
+	 * Each individual server in the array must include a domain and port, with an optional
+	 * weight value: $servers = array( array( '127.0.0.1', 11211, 0 ) );
+	 *
+	 * @link    http://www.php.net/manual/en/memcached.addservers.php
+	 *
+	 * @param \Memcached $memcached Memcached instance.
+	 * @param array      $servers   Array of server to register.
+	 *
+	 * @return bool True on success; false on failure.
+	 */
+	protected function addServers( \Memcached $memcached, array $servers ) {
+		if ( $memcached->isPersistent() ) {
+			$servers = $this->getUnusedServers( $memcached, $servers );
 		}
 
-		return $servers;
+		if ( ! empty( $servers ) ) {
+			return $memcached->addServers( $servers );
+		}
+
+		// If Memcached is not persistent but we had no servers to add, return false.
+		return $memcached->isPersistent();
+	}
+
+	/**
+	 * Filters out used servers from a list
+	 *
+	 * @param \Memcached $memcached Memcached instance.
+	 * @param array      $servers   List of servers to filter out used ones from.
+	 *
+	 * @return array List of unused server.
+	 */
+	protected function getUnusedServers( \Memcached $memcached, array $servers ) {
+		$unused = $servers;
+
+		$listed = $memcached->getServerList();
+		if ( ! empty( $listed ) ) {
+			// Reset unused to empty list.
+			$unused = [];
+
+			foreach ( $servers as $server ) {
+
+				$test = [
+					'host'   => $server[0],
+					'port'   => isset( $server[1] ) ? (int) $server[1] : 11211,
+					'weight' => isset( $server[2] ) ? (int) $server[2] : 0,
+				];
+
+				if ( in_array( $test, $listed, true ) ) {
+					continue;
+				}
+
+				$unused[] = $server;
+			}
+		}
+
+		return $unused;
 	}
 }
